48a9894801a286c16ad9e5e5461e4eca389bbeee
Niaz M. Sameer <niazmohsameer@gmail.com>
2020-12-02 20:14:33 +0800

    Initial Commit

---
+++ CppLearn/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppLearn
+//
+//  Created by Sameer Niaz Mohammad on 2/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
